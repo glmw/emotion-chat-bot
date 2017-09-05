b3b668b8ec02f7c987c7013f5e6ef06b06c5f305
kelvin <loymw@naver.com>
2017-09-05 22:48:04 +0900

    init

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from microsoftbotframework import MsBot
 from tasks import *
 import os
 
-bot = MsBot(port=int(os.environ['PORT']))
+#bot = MsBot(port=int(os.environ['PORT']))
+bot = MsBot()
 bot.add_process(echo_response)
 bot.run()
